493a1310ec010b82017a8efd3c5d27a082757ccb
Oliver Lin <jumpy12359@gmail.com>
2026-01-02 23:31:30 +1100

    :sparkles: Add new KemoDog model and commission

---
+++ data/models/KemoDog.ts
@@ -0,0 +1,8 @@
+export const KemoDog = {
+  modelName: 'KemoDog',
+  dbName: 'KemoDog',
+  coverImage: 'https://booth.pximg.net/aa6068dc-db1f-43cc-9092-da43b5272f10/i/7066346/2438d1f7-e4c4-4ecb-978f-0f775560f7e8.png',
+  bgColor: '#005954',
+  officialLink: 'https://speed.booth.pm/items/7066346',
+  featured: false,
+} as const;
