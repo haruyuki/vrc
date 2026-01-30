d08556892f82d414d1e4803cb19ba3094cbe0c2b
Oliver Lin <jumpy12359@gmail.com>
2026-01-30 21:53:44 +1100

    :bug: Try fix Rosune cover image link

--- data/models/Rosune.ts
+++ data/models/Rosune.ts
@@ -1,7 +1,7 @@
 export const Rosune = {
   modelName: 'Rosune',
   dbName: 'Rosune',
-  coverImage: 'https://pbs.twimg.com/media/Ganf_FkaYAAJZdY?format=jpg&name=medium',
+  coverImage: 'https://pbs.twimg.com/media/Gj_6bt-aIAAzqFK.jpg',
   bgColor: '#21292c',
   officialLink: 'https://www.patreon.com/HazeV',
   featured: false,
