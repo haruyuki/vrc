0613cb26a6b03b348f0e3546c339be8f5273224a
Oliver Lin <jumpy12359@gmail.com>
2026-01-30 23:05:15 +1100

    :bug: Use local link for cover image

--- data/models/Rosune.ts
+++ data/models/Rosune.ts
@@ -1,7 +1,7 @@
 export const Rosune = {
   modelName: 'Rosune',
   dbName: 'Rosune',
-  coverImage: 'https://pbs.twimg.com/media/Gj_6bt-aIAAzqFK.jpg',
+  coverImage: '/assets/images/covers/Rosune.png',
   bgColor: '#21292c',
   officialLink: 'https://www.patreon.com/HazeV',
   featured: false,
